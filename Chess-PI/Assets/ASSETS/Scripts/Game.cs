cd683d588403bd39146914bb6449074706982631
mrc300 <manelfcunha@gmail.com>
2023-11-10 19:20:39 +0000

    gerador do tabuleiro

---
+++ Chess-PI/Assets/ASSETS/Scripts/Game.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour
+{
+    public GameObject brownTile;
+    public GameObject lightTile;
+
+   
+    void Start()
+    {
+        createBoard();
+    }
+
+
+
+
+    void createBoard(){
+        Vector3 newVector = new Vector3(-31.53f,-31.53f,0);
+        for(int x=0; x<8; x++){
+            for(int y=0; y<8; y++){
+            if((x+y)%2==0){
+             Instantiate(brownTile,newVector + new Vector3(x*(9.01f),y*(9.01f),-1), Quaternion.identity);
+            }else{
+             Instantiate(lightTile,newVector + new Vector3(x*(9.01f),y*(9.01f),-1), Quaternion.identity);   
+            }
+            } 
+        }
+    }
+
+}
